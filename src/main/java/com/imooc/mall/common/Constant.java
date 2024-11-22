c2ad28f255e78536883d84bb470b1dcaa75cb060
Donkor <donkor@yeah.net>
2024-11-22 17:46:35 +0800

    注册接口，md5密码加盐

---
+++ src/main/java/com/imooc/mall/common/Constant.java
@@ -0,0 +1,9 @@
+package com.imooc.mall.common;
+
+/**
+ * 常量值
+ */
+public class Constant {
+    //盐值
+    public static final String SALT = "s0jx090a92IS]a.,qWc";
+}
--- src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
+++ src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
@@ -5,8 +5,11 @@ import com.imooc.mall.exception.ImoocMallExceptionEnum;
 import com.imooc.mall.model.dao.UserMapper;
 import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.service.UserService;
+import com.imooc.mall.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
 
 /**
  * UserService实现类
@@ -31,10 +34,14 @@ public class UserServiceImpl implements UserService {
         //写到数据库
         User user = new User();
         user.setUsername(userName);
-        user.setPassword(password);
+        try {
+            user.setPassword(MD5Utils.getMD5Str(password));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
-        int count=userMapper.insertSelective(user);
-        if(count==0){
+        int count = userMapper.insertSelective(user);
+        if (count == 0) {
             throw new ImoocMallException(ImoocMallExceptionEnum.INSERT_FAILED);
         }
     }
